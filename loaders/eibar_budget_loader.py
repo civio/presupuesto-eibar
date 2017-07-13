299038b89ba9a8d0cfe37e7aa7d6825260339ec9
Eduardo Sebastian <esebastian@altiora.es>
2017-07-13 13:33:38 +0200

    Update budget loader

--- loaders/eibar_budget_loader.py
+++ loaders/eibar_budget_loader.py
@@ -8,67 +8,86 @@ import re
 
 class EibarBudgetLoader(SimpleBudgetLoader):
 
+    # Parse an input line into fields
     def parse_item(self, filename, line):
         # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
         # mapping to be constant over time, we are forced to amend budget data prior to 2015.
         # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
+        # TODO: it seems that the specific regional laws apply in a different way
         programme_mapping = {
-            # old programme: new programme
-            '1340': '1350',     # Protección Civil
-            '1350': '1360',     # Extinción de incendios
-            '1550': '1532',     # Vías públicas
-            '1620': '1621',     # Recogida de residuos
-            '3130': '3110',     # Protección de la salud
-            '3210': '3219',     # Educación preescolar y primaria
-            '3220': '3229',     # Enseñanza secundaria
-            '3230': '3239',     # Promoción educativa
-            '3240': '3260',     # Servicios complementarios de educación
-            '3340': '3341',     # Promoción cultural
-            '4410': '4411',     # Promoción, mantenimiento y desarrollo del transporte
-            '4940': '4911',     # URBAN- Arona 2007-2013
-        }
-        programme_mapping_2015 = {
-            '3340': '3341',     # Promoción cultural
+        # old programme: new programme
         }
 
-        # Some dirty lines in input data
-        if line[0]=='':
-            return None
+        # Institutional code (all income goes to the root node)
+        ic_code = '000'
 
+        # Description
+        description = line[3].replace('..', '. ')
+        description = description.strip()
+        if not re.search('[A-Z]\.$',description):
+            description = description.rstrip('.')
+
+        # Type of data
         is_expense = (filename.find('gastos.csv')!=-1)
         is_actual = (filename.find('/ejecucion_')!=-1)
+
+        # Expenses
         if is_expense:
-            # The input data combines functional and economic codes in a very unusual way
-            match = re.search('^ *(\d+) +(\d+) *', line[0])
-            # We got 3- or 4- digit functional codes as input, so add a trailing zero
-            fc_code = match.group(1).ljust(4, '0')
-            ec_code = match.group(2)
+            # We got a composite identifier, with more info, so we must isolate the functional code
+            fc_code = line[0].strip()
+            fc_code = fc_code[14:20]
+            fc_code = fc_code.replace('.', '')
 
             # For years before 2015 we check whether we need to amend the programme code
             year = re.search('municipio/(\d+)/', filename).group(1)
             if int(year) < 2015:
                 fc_code = programme_mapping.get(fc_code, fc_code)
-            elif int(year) == 2015:
-                fc_code = programme_mapping_2015.get(fc_code, fc_code)
+
+            # We got a composite identifier, with more info, so we must isolate the economic code
+            # On economic codes we get the first three digits (everything but last two)
+            ec_code = line[0].strip()
+            ec_code = ec_code[7:10]
+
+            # Item numbers are the last two digits from the economic codes (fourth and fifth digit)
+            item_number = line[0].strip()
+            item_number = item_number[11:13]
+
+            # Parse amount
+            amount = line[10 if is_actual else 5].strip()
+            amount = self._parse_amount(amount)
 
             return {
                 'is_expense': True,
                 'is_actual': is_actual,
                 'fc_code': fc_code,
-                'ec_code': ec_code[:-2],        # First three digits (everything but last two)
-                'ic_code': '000',
-                'item_number': ec_code[-2:],    # Last two digits
-                'description': line[1],
-                'amount': self._parse_amount(line[5 if is_actual else 2])
+                'ec_code': ec_code,
+                'ic_code': ic_code,
+                'item_number': item_number,
+                'description': description,
+                'amount': amount
             }
 
+        # Income
         else:
+            # We got a composite identifier, with more info, so we must isolate the economic code
+            # On economic codes we get the first three digits (everything but last two)
+            ec_code = line[0].strip()
+            ec_code = ec_code[2:5]
+
+            # Item numbers are the last two digits from the economic codes (fourth and fifth digit)
+            item_number = line[0].strip()
+            item_number = item_number[6:8]
+
+            # Parse amount
+            amount = line[5 if is_actual else 4].strip()
+            amount = self._parse_amount(amount)
+
             return {
                 'is_expense': False,
                 'is_actual': is_actual,
-                'ec_code': line[0][:-2],        # First three digits
-                'ic_code': '000',               # All income goes to the root node
-                'item_number': line[0][-2:],    # Fourth and fifth digit; careful, there's trailing dirt
-                'description': line[1],
-                'amount': self._parse_amount(line[5 if is_actual else 2])
+                'ec_code': ec_code,
+                'ic_code': ic_code,
+                'item_number': item_number,
+                'description': description,
+                'amount': amount
             }
