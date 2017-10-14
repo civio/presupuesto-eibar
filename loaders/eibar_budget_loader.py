25a9c596f1d3c22ecf99baa36bc9cb88a107d936
Eduardo Sebastian <esebastian@altiora.es>
2017-10-14 17:18:39 +0200

    Complete programme mappings

--- loaders/eibar_budget_loader.py
+++ loaders/eibar_budget_loader.py
@@ -16,46 +16,40 @@ class EibarBudgetLoader(SimpleBudgetLoader):
         # IMPORTANT: The specific regional laws apply in a different way
         programme_mapping = {
         # old programme: new programme
-            '01000':'01100',    # Deuda pública
             '11100':'91200',    # Órganos de gobierno
-            '12110':'92011',    # Secretaría general # TODO: verificar, puede que se divida en varios
+            '12110':'92011',    # Secretaría general
             '12120':'92310',    # Gestión del padrón municipal de habitantes
             '12130':'92500',    # Atención a los ciudadanos
             '12140':'92020',    # Administración de personal
             '12150':'92030',    # Informática
-            '12160':'92040',    # Edificios polivalentes # TODO: verificar, ya hay un mapeo
-            '12170':'13000',    # Admón. general de la seguridad y protección civil # TODO: verificar, puede que se divida en varios
+            '12160':'92040',    # Edificios polivalentes
+            '12170':'15340',    # Brigada *
             '12180':'33610',    # Ego-ibarra
             '19000':'92900',    # Imprevistos y funciones no clasificadas
-            '22210':'13200',    # Seguridad ciudadana # TODO: verificar, puede que se divida en varios
-            '31310':'23160',    # Atención a personas en situación de riesgo, desprotección, s. # TODO: verificar, puede que se divida en varios
-            '31320':'92040',    # Edificios polivalentes # TODO: verificar, ya hay un mapeo
+            '31320':'23140',    # Apartamentos tutelados *
             '31321':'23150',    # Promoción de la igualdad de género
             '31322':'23170',    # Actuaciones contra la drogodependencia
-            '31323':'23160',    # Atención a personas en situación de riesgo, desprotección, s. # TODO: verificar, puede que se divida en varios
+            '31323':'23160',    # Atención a personas en situación de riesgo, desprotección, s
             '31324':'23180',    # Diversidad cultural, integración emigrantes
-            '31350':'23130',    # Ayudas de emergencia social (A.E.S.) # TODO: verificar, puede que se divida en varios
-            '31390':'23010',    # Evaluación e información de situaciones de necesidad
-            '31400':'22100',    # Otras prestaciones económicas a favor de empleados # TODO: verificar, puede que se divida en varios
+            '31350':'23020',    # Cooperación y ayudas el desarrollo (ONG)
+            '31400':'22100',    # Otras prestaciones económicas a favor de empleados
             '32200':'24100',    # Fomento de empleo
-            '32310':'23140',    # Apartamentos tutelados
+            '32310':'32410',    # Programa cualificación profesional inicial (PCPI)
             '41320':'31170',    # Campañas de desinfección, desinsectación y desratización
-            '42200':'32300',    # Funcionamiento de centros docentes
-            '43110':'15220',    # Conservación y rehabilitación de la edificación # TODO: verificar, puede que se divida en varios
+            '42200':'32300',    # Funcionamiento de centros docentes *
+            '43110':'15220',    # Conservación y rehabilitación de la edificación
             '43210':'15100',    # Urbanismo
             '43300':'16500',    # Alumbrado público
             '43400':'17100',    # Parques y jardines
-            '43500':'13400',    # Movilidad urbana
+            '43500':'13400',    # Movilidad urbana *
             '43600':'15330',    # Equipamiento urbano
             '44200':'16300',    # Limpieza viaria
             '44300':'16400',    # Cementerio
             '44500':'49300',    # Protección de consumidores y usuarios
             '44600':'44110',    # Transporte colectivo urbano de viajeros
-            '44900':'13320',    # Mantenimiento señalización y sistema aparcamiento # TODO: verificar, puede que se divida en varios
             '45110':'33000',    # Administración general de cultura
             '45130':'33210',    # Bibliotecas públicas
             '45140':'33410',    # Unidad música
-            '45150':'33421',    # Unidad de artes escénicas # TODO: verificar, puede que se divida en varios
             '45151':'33422',    # Jornadas de teatro
             '45160':'33430',    # Unidad de arte
             '45180':'33330',    # Museo
@@ -67,14 +61,12 @@ class EibarBudgetLoader(SimpleBudgetLoader):
             '45520':'33520',    # Euskaltegia
             '45610':'33720',    # Juventud
             '45620':'33710',    # Hogares de jubilados
-            '51100':'15320',    # Pavimentación de vías públicas
-            '53100':'45400',    # Caminos vecinales # TODO: verificar, puede que se divida en varios
-            '53300':'17200',    # Protección y mejora del medio ambiente # TODO: verificar, puede que se divida en varios
-            '61100':'93110',    # Presupuestos y contabilidad # TODO: verificar, puede que se divida en varios
+            '53100':'45400',    # Caminos vecinales
+            '53300':'17200',    # Protección y mejora del medio ambiente
             '62220':'43120',    # Mercados, abastos y lonjas
             '76010':'43310',    # Desarrollo empresarial e innovación
-            '76020':'43200',    # Información y promoción turística # TODO: verificar, no está en su lista de programas
-            '76030':'43320',    # Urb Grade
+            '76020':'43200',    # Información y promoción turística
+            '76030':'43320',    # Urb. Grade.
         }
 
         # Institutional code (all income goes to the root node)
