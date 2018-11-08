7272281469dedf14315e6746e921fce3137c2e1e
Eduardo Sebastian <esebastian@altiora.es>
2018-11-08 23:59:03 +0100

    Remove obsolete setting
    civio/presupuesto-management#603

--- settings.py
+++ settings.py
@@ -146,8 +146,6 @@ OVERVIEW_USE_NEW_VIS = True
 # Note: 0.5 usually leaves nodes ordered as defined. 0.95 sorts by size (decreasing).
 # OVERVIEW_RELAX_FACTOR = 0.5
 
-# Adjust inflation in amounts in Overview page. Default: True
-# ADJUST_INFLATION_IN_OVERVIEW = False
 
 # Show Subtotals panel in Overview. Default: False
 # SHOW_OVERVIEW_SUBTOTALS = True
