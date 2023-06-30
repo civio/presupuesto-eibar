6a57d0b90118df6cd07a4a9015da1c1eefac7dcf
David Cabo <david.cabo@gmail.com>
2023-06-30 22:37:57 +0200

    Replace Google Analytics with Plausible civio/presupuesto-management#1269

--- settings.py
+++ settings.py
@@ -81,9 +81,8 @@ LANGUAGES = (
   # ('gl', 'Galego'),
 )
 
-# Google Analytics ID. Default: ''
-# In order to get the ID create a Google Analytics Acount in https://analytics.google.com/analytics/web/
-ANALYTICS_ID            = 'UA-28946840-35'
+# Plausible data domain. Default: ''
+PLAUSIBLE_DOMAIN        = 'eibar.eus'
 
 # Setup Data Source Budget link
 DATA_SOURCE_BUDGET      = 'http://www.eibar.eus/es/ayuntamiento/presupuestos'
