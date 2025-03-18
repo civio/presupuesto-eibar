72af707d23c7fdac705edd3780f7dcaaa83580ac
David Cabo <david.cabo@gmail.com>
2025-03-19 00:16:30 +0100

    Make theme compatible with Python 3 civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,1 +1,6 @@
-from eibar_budget_loader import EibarBudgetLoader
+import six
+
+if six.PY2:
+    from eibar_budget_loader import EibarBudgetLoader
+else:
+    from .eibar_budget_loader import EibarBudgetLoader
